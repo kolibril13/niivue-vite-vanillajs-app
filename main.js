4172c68909f1862aa20c584f656a7c7871428fb9
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-01-12 18:19:59 +0100

    refactor appendChild

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-// import "./style.css";
-
 import { Niivue } from "@niivue/niivue";
 
-document.querySelector("#app").innerHTML = `
-  <div style="position: absolute; top: 0; left: 0;"> 
-    <canvas id="gl" width="800" height="800"></canvas>
-  </div>
-`;
+// Create a canvas element
+const canvas = document.createElement("canvas");
+canvas.id = "gl";
+canvas.width = 800;
+canvas.height = 800;
 
-// make an array of volumes to load
+// Append the canvas to the element with id="app"
+document.querySelector("#app").appendChild(canvas);
+
+// Make an array of volumes to load
 let volumeList = [
-  { url: "https://niivue.github.io/niivue-demo-images/mni152.nii.gz" },
+    { url: "https://niivue.github.io/niivue-demo-images/mni152.nii.gz" },
 ];
+
 const nv = new Niivue({ isResizeCanvas: false });
 nv.attachTo("gl"); // attach to canvas with id="gl"
 nv.loadVolumes(volumeList);
