e75018c66106f3fd33e65e7996c68e5fe924494f
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-01-16 17:07:05 +0100

    add slider

--- main.js
+++ main.js
@@ -4,6 +4,32 @@ const canvas = document.createElement("canvas");
 canvas.width = 800;
 canvas.height = 1500;
 
+
+var label = document.createElement("label");
+label.setAttribute("for", "meshSlider");
+label.textContent = "Threshold";
+label.style.color = "white";
+label.style.fontSize = "25px";
+// Create input
+var input = document.createElement("input");
+input.setAttribute("type", "range");
+input.setAttribute("min", "1");
+input.setAttribute("max", "49");
+input.setAttribute("value", "20");
+input.setAttribute("class", "slider");
+input.setAttribute("id", "meshSlider");
+input.style.width = "400px";
+
+// Append elements to #app
+document.querySelector("#app").appendChild(label);
+document.querySelector("#app").appendChild(input);
+
+
+var slider = document.getElementById("meshSlider");
+slider.oninput = function () {
+  nv1.setMeshLayerProperty(nv1.meshes[0].id, 0, "cal_min", this.value * 0.1);
+};
+
 document.querySelector("#app").appendChild(canvas);
 
 var nv1 = new Niivue({
@@ -25,5 +51,4 @@ var meshLHLayersList1 = [
 nv1.loadMeshes([
   { url: "images/BrainMesh_ICBM152.lh.mz3", layers: meshLHLayersList1 },
 ]);
-// nv1.setMeshShader(nv1.meshes[0].id, "Matcap");
 nv1.setClipPlane([-0.1, 270, 0]);
