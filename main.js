d868560bd66975a6edb9fe996c1d07c26c996503
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-01-16 12:05:56 +0100

    add two volumes

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 import { Niivue } from "@niivue/niivue";
 
-// Create a canvas element
 const canvas = document.createElement("canvas");
 canvas.id = "gl";
 canvas.width = 800;
 canvas.height = 800;
 
-// Append the canvas to the element with id="app"
 document.querySelector("#app").appendChild(canvas);
 
-// Make an array of volumes to load
-let volumeList = [
-    { url: "https://niivue.github.io/niivue-demo-images/mni152.nii.gz" },
+var volumeList1 = [
+  {
+    url: "images/mni152.nii.gz",
+    colormap: "gray",
+    visible: true,
+    opacity: 1,
+  },
+  {
+    url: "images/hippo.nii.gz",
+    colormap: "green",
+    visible: true,
+    opacity: 1,
+  },
 ];
 
 const nv = new Niivue({ isResizeCanvas: false });
-nv.attachTo("gl"); // attach to canvas with id="gl"
-nv.loadVolumes(volumeList);
+nv.attachTo("gl");
+nv.loadVolumes(volumeList1);
