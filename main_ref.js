76eac6b87494bf28185c2b050680ce235be54428
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-01-31 15:56:31 +0100

    add highlight js

--- main_ref.js
+++ main_ref.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 // Create main container
 const container = document.createElement("div");
 container.style.display = "flex";
@@ -49,3 +54,30 @@ canvasContainer.appendChild(canvas);
 container.appendChild(codeBlock);
 container.appendChild(canvasContainer);
 
+// Append the main container to the body of the document
+// document.body.appendChild(container);
+
+document.querySelector("#app").appendChild(container);
+import { Niivue } from "@niivue/niivue";
+
+var volumeList1 = [
+  {
+    url: "mni152.nii.gz",
+    colormap: "gray",
+    visible: true,
+    opacity: 1,
+  },
+];
+
+const nv = new Niivue({ isResizeCanvas: false });
+nv.attachTo("gl");
+
+// Use .then() to wait for the volume to load
+nv.loadVolumes(volumeList1).then(() => {
+  console.log("Volume loaded");
+  console.log("hi" + nv.colormaps());
+  nv.setOpacity(0, 0.5);
+  nv.setColorMap(nv.volumes[0].id, "red");
+}).catch((error) => {
+  console.error("Error loading volumes", error);
+});
