aa001c52c5057820c272c657d7370432e54affa0
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-01-31 16:24:33 +0100

    make a create container function

--- main.js
+++ main.js
@@ -2,16 +2,8 @@ import hljs from "highlight.js";
 import "highlight.js/styles/atom-one-dark.css";
 import { Niivue } from "@niivue/niivue";
 
-// Create main container
-const container = document.createElement("div");
-container.style.display = "flex";
-container.style.justifyContent = "space-between";
-
-// Create code block container
-const codeBlock = document.createElement("pre");
-const codeElement = document.createElement("code");
-codeElement.className = "javascript";
-codeElement.textContent = `import { Niivue } from "@niivue/niivue";
+const code = `
+import { Niivue } from "@niivue/niivue";
 
 var volumeList1 = [
   {
@@ -25,46 +17,39 @@ var volumeList1 = [
 const nv = new Niivue({ isResizeCanvas: false });
 nv.attachTo("gl");
 
-// Use .then() to wait for the volume to load
-nv.loadVolumes(volumeList1).then(() => {
-  console.log("Volume loaded");
-  console.log("hi" + nv.colormaps());
-  nv.setOpacity(0, 0.5);
-  nv.setColorMap(nv.volumes[0].id, "red");
-}).catch((error) => {
-  console.error("Error loading volumes", error);
-});
+nv.loadVolumes(volumeList1)
+  .then(() => {
+    console.log("Volume loaded");
+    console.log("hi" + nv.colormaps());
+    nv.setOpacity(0, 0.5);
+    nv.setColorMap(nv.volumes[0].id, "red");
+  })
+  .catch((error) => {
+    console.error("Error loading volumes", error);
+  });
 `;
-codeBlock.appendChild(codeElement);
-codeBlock.style.display = "flex";
-codeBlock.style.justifyContent = "center";
 
-codeBlock.style.width = "50%";
-codeBlock.style.whiteSpace = "pre-wrap";
-hljs.highlightElement(codeElement);
+const createContainer = (width, content) => {
+  const container = document.createElement("div");
+  container.style.width = width;
+  container.style.display = "flex";
+  container.style.justifyContent = "center";
+  container.innerHTML = content;
+  return container;
+};
 
-container.appendChild(codeBlock);
+const codeContainer = createContainer("50%", `<pre><code class="javascript">${code}</code></pre>`);
+hljs.highlightElement(codeContainer.querySelector("code"));
 
-// Create and configure canvas
+const canvasContainer = createContainer("50%", '<canvas id="gl"></canvas>');
 
-const canvasContainer = document.createElement("div");
-canvasContainer.style.width = "50%";
-const canvas = document.createElement("canvas");
-canvas.id = "gl";
+document.querySelector("#app").appendChild(codeContainer);
+document.querySelector("#app").appendChild(canvasContainer);
 
-canvasContainer.appendChild(canvas);
-
-// Append the containers to the main container
-container.appendChild(canvasContainer);
-
-// Append the main container to the document
-document.querySelector("#app").appendChild(container);
-
-// Initialize and Execute Niivue
 const nv = new Niivue({ isResizeCanvas: true });
 nv.attachTo("gl");
 
-var volumeList1 = [
+const volumeList1 = [
   {
     url: "mni152.nii.gz",
     colormap: "gray",
