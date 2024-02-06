5e66d8fdb065651865cd52be69f41cdeec5f426b
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-02-06 15:21:23 +0100

    tweak

--- main.js
+++ main.js
@@ -1,73 +1,25 @@
 import hljs from "highlight.js";
 import "highlight.js/styles/atom-one-dark.css";
-import { Niivue } from "@niivue/niivue";
 
 const code = `
-import { Niivue } from "@niivue/niivue";
-
-var volumeList1 = [
-  {
-    url: "mni152.nii.gz",
-    colormap: "gray",
-    visible: true,
-    opacity: 1,
-  },
-];
-
-const nv = new Niivue({ isResizeCanvas: false });
-nv.attachTo("gl");
-
-nv.loadVolumes(volumeList1)
-  .then(() => {
-    console.log("Volume loaded");
-    console.log("hi" + nv.colormaps());
-    nv.setOpacity(0, 0.5);
-    nv.setColorMap(nv.volumes[0].id, "red");
-  })
-  .catch((error) => {
-    console.error("Error loading volumes", error);
-  });
+i = 3;
+console.log(i);
 `;
 
-const createContainer = (width, content) => {
+const createContainer = (content, width) => {
   const container = document.createElement("div");
   container.style.width = width;
-  container.style.display = "inline-block";
   container.innerHTML = content;
   return container;
 };
 
-const codeContainer = createContainer("50%", `<pre><code class="javascript">${code}</code></pre>`);
+const codeContainer = createContainer(
+  `<pre><code class="javascript">${code}</code></pre>`,
+  `50%`
+);
 hljs.highlightElement(codeContainer.querySelector("code"));
 
-const canvasContainer = createContainer("50%", '<canvas id="gl"></canvas>');
-
 const mainContainer = document.createElement("div");
-mainContainer.style.display = "flex";
 mainContainer.appendChild(codeContainer);
-mainContainer.appendChild(canvasContainer);
 
 document.querySelector("#app").appendChild(mainContainer);
-
-const nv = new Niivue({ isResizeCanvas: true });
-nv.attachTo("gl");
-
-const volumeList1 = [
-  {
-    url: "mni152.nii.gz",
-    colormap: "gray",
-    visible: true,
-    opacity: 1,
-  },
-];
-
-nv.loadVolumes(volumeList1)
-  .then(() => {
-    console.log("Volume loaded");
-    console.log("hi" + nv.colormaps());
-    nv.setOpacity(0, 0.5);
-    nv.setColorMap(nv.volumes[0].id, "red");
-  })
-  .catch((error) => {
-    console.error("Error loading volumes", error);
-  });
